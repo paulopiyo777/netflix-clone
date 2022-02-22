5d84b90a66e304d264e3ba9141dc2db64cf55492
paulopiyo777 <paulopiyo777@gmail.com>
2022-02-22 13:42:24 -0600

    Create starter files for app

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAj1yr4sWe1pqpt7k35WfYFxkJE45IlKoc",
+  authDomain: "netflix-clone-4f7bf.firebaseapp.com",
+  projectId: "netflix-clone-4f7bf",
+  storageBucket: "netflix-clone-4f7bf.appspot.com",
+  messagingSenderId: "401366200095",
+  appId: "1:401366200095:web:9867e6bf6da43b5d578a2a",
+};
