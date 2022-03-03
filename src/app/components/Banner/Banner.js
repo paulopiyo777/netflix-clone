ad9f42391f75af0175ceb602f9907ce7d0e86c1a
paulopiyo777 <paulopiyo777@gmail.com>
2022-03-03 13:51:22 -0600

    Add HomeScreen components and fetch data from TMDB

--- src/app/components/Banner/Banner.js
+++ src/app/components/Banner/Banner.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Banner.css";
+import axios from "../../api/axios";
+import requests from "../../api/Request";
 
 function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      // use created axios instance not global axios libraray
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      // set a random movie from the data received
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+
+    fetchData();
+  }, []);
+
+  console.log(movie);
+
   function truncate(string, charLimit) {
     return string?.length > charLimit
       ? string.substr(0, charLimit - 1) + "..."
@@ -13,18 +35,20 @@ function Banner() {
       className="banner"
       style={{
         backgroundSize: "cover",
-        backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png")`,
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         backgroundPosition: "center center",
       }}
     >
       <div className="banner__contents">
-        <h1 className="banner__title">Movie Name</h1>
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
         <h1 className="banner__description">
-          {truncate(`Test Description`, 150)}
+          {truncate(`${movie?.overview}`, 150)}
         </h1>
       </div>
 
