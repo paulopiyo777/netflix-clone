2d338e1408cea3a3a06ea80b5ad801978373f628
paulopiyo777 <paulopiyo777@gmail.com>
2022-03-04 00:33:23 -0600

    Create routes for different application pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "./App.css";
 import HomeScreen from "./app/components/HomeScreen/HomeScreen";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginScreen from "./app/components/LoginScreen/LoginScreen";
 
 function App() {
+  const user = null;
+
   return (
     <div className="app">
-      <HomeScreen />
+      <Router>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        )}
+      </Router>
     </div>
   );
 }
